97d486bc4d6af4e5a06da7d8b2239d801aa06329
Francesco Meloni <francesco.meloni03@icloud.com>
2023-05-17 17:35:30 +0200

    html/js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-const { createApp }  = Vue;
 
+const { createApp } = Vue
 createApp({
     data(){
         return{
-            
+            emails: [],
+            linkEmail: "https://flynn.boolean.careers/exercises/api/random/mail",
         }
-    },
-    mounted(){
+    }, 
+    mounted() {
+            for (let c = 0; c < 10; c++) {
 
+                axios.get(this.linkEmail).then((answer) => {
+                    this.emails.push(answer.data.response)
+                    console.log(this.emails)
+                    
+                })      
+        }
     }
-}).mount("#app");
+}).mount('#app')
